bb2f367ad2715ccfd1c04483b4840e12c820807a
王增凯 <zengkai.wang@irootech.com>
2018-07-05 14:53:04 +0800

    安装数据库插件并配置连接mongodb

--- config/config.default.js
+++ config/config.default.js
@@ -1,2 +1,19 @@
 // config/config.default.js
-exports.keys = 'sfjslfjlsfjldf';
+module.exports = appInfo=> {
+	const config = exports = {};
+
+	config.keys = 'sfjslfjlsfjldf';
+
+	//配置数据库插件
+	config.mongoose = {
+		url: 'mongodb://127.0.0.1:27017/blog',
+		option: {
+			server: {
+				poolSize: 20
+			}
+		}
+	};	
+
+	return config;
+
+}
---
+++ config/plugin.js
@@ -0,0 +1,5 @@
+//开启插件,挂在到this.ctx上
+exports.mongoose = {
+	enable: true,
+	package: 'egg-mongoose'
+}
