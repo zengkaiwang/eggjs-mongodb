a49d7f938b3e7e538eb70fa35f5df6c487e5a7f5
王增凯 <zengkai.wang@irootech.com>
2018-07-05 15:06:19 +0800

    创建model目录service目录连接数据库发布模型已便操作数据库

---
+++ app/model/post.js
@@ -0,0 +1,13 @@
+module.exports = app => {
+	const mongoose = app.mongoose;
+
+	const Schema = mongoose.Schema;
+
+	const PostSchema = new Schema({
+		title: { type: String },
+		content: { type: String },
+		description: { type: String }
+	});
+
+	return mongoose.model('Post', PostSchema);
+}
